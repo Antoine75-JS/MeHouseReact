6b3049ac5c977dfd60fce5dddbcc6dce918cfc0f
Antoine75-JS <hello@antoinegarcia.com>
2021-07-28 11:29:54 +0200

    integration Modal Add Orga

---
+++ src/components/CreateOrgaModal/index.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// Validation props
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// Components
+import Toast from 'src/containers/Toast';
+import Loading from 'src/components/Loading';
+
+import './styles.scss';
+
+// Yup validation schema
+const orgaSchema = yup.object().shape({
+  orgName: yup.string().required().typeError("Le nom de l'organisation doit contenir entre 3 et 30 caractères alphanumériques").matches(/^[a-zA-Z0-9]{3,30}$/),
+});
+
+// Component
+const CreateOrgaModal = () => {
+  // Temp local state
+  const [isOpen, setIsOpen] = useState(true);
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(orgaSchema),
+  });
+
+  const handleCreateOrga = (data) => {
+    console.log(data);
+  };
+
+  // Handle button close modal
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  // Handle listener esc key
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
+  // Handle error message in Toast
+  useEffect(() => {
+    if (errors.orgName) {
+      console.log(errors);
+    }
+  }, [errors]);
+
+  return (
+    <>
+      {isOpen && (
+        <div className="createOrga">
+          <div className="createOrga-closeModal" onClick={handleCloseModal}>
+            <p className="createOrga-closeModal--cross">+</p>
+          </div>
+          <form onSubmit={handleSubmit(handleCreateOrga)} className="createOrga-form">
+            <label htmlFor='orgName' className="createOrga-form--title">Nom de l'organisation</label>
+            <input {...register('orgName')} type="text" name="orgName" className="createOrga-form--input" />
+            <input type="submit" value="Créer" className="createOrga-form--btn" />
+            <p className="createOrga-form--errors">{errors.orgName?.message}</p>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CreateOrgaModal;
--- src/components/OrgasListComponent/index.js
+++ src/components/OrgasListComponent/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
 import api from 'src/api';
+import CreateOrgaModal from 'src/components/CreateOrgaModal';
 
+// Components
 import './styles.scss';
 
 const OrgasListComponent = () => {
@@ -24,23 +26,31 @@ const OrgasListComponent = () => {
       });
   }, []);
 
+  const handleAddOrganization = () => {
+    console.log('Adding Organization');
+  };
+
   return (
-    <div className="organisations">
-      {!loading && organisations.length > 0 ? (
-        organisations.map((organisation) => (
-          <div className="organisations-card" key={organisation._id}>
-            <h2 className="organisations-card--title">
-              {organisation.orgName}
-            </h2>
-            <h3 className="organisations-card--nbCat">
-              {organisation.orgCategories?.length} catégories
-            </h3>
-          </div>
-        ))
-      ) : (
-        <h2>No organisations</h2>
-      )}
-    </div>
+    <>
+      <div className="organisations">
+        {!loading && organisations.length > 0 ? (
+          organisations.map((organisation) => (
+            <div className="organisations-card" key={organisation._id}>
+              <h2 className="organisations-card--title">
+                {organisation.orgName}
+              </h2>
+              <h3 className="organisations-card--nbCat">
+                {organisation.orgCategories?.length} catégories
+              </h3>
+            </div>
+          ))
+        ) : (
+          <h2>No organisations</h2>
+        )}
+        <div className="addOrganizationBtn" onClick={handleAddOrganization}>+</div>
+      </div>
+      <CreateOrgaModal />
+    </>
   );
 };
 
