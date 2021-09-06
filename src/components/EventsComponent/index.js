6de3497e12479873bccf035e06f4b49306571205
Antoine75-JS <hello@antoinegarcia.com>
2021-09-06 15:35:30 +0200

    addEvent component

--- src/components/EventsComponent/index.js
+++ src/components/EventsComponent/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+
+// Validation props
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// Date picker
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 // Components
 import ExpirationChip from 'src/components/ExpirationChip';
+import AddButtonFilled from 'src/components/Utils/AddButtonFilled';
 
 import {
   FiTrash,
@@ -9,19 +19,53 @@ import {
 
 import './styles.scss';
 
+// Yup validation schema
+const eventSchema = yup.object().shape({
+  // eslint-disable-next-line newline-per-chained-call
+  eventName: yup.string().required().typeError("Le nom de l'événement doit contenir entre 3 et 30 caractères alphanumériques").min(3).max(30),
+  // eventDate: yup.date().required().typeError("La date de l'événement doit être coorecte"),
+});
+
 const EventsComponent = () => {
-  const isEmpty = true;
+  // React hook form
+  const { control, register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(eventSchema),
+  });
+
+  // Local states
+  const [eventDate, setEventDate] = useState(new Date());
+
+  const handleEventDateChange = (date) => {
+    setEventDate(date);
+    console.log(date, eventDate);
+  };
+
+  const handleNewEventSubmit = (data) => {
+    console.log(data, eventDate);
+  };
 
   return (
     <div className="eventsComponent">
-      <div className="eventsComponent-title">Événements</div>
-      <div className="eventsComponent-events">
-        <div className="eventsComponent-events_event">
-          <div className="eventsComponent-events_event--title">Concert</div>
-          <div className="eventsComponent-events_event--date">01/01/2021</div>
-          <ExpirationChip expireDate={"01/01/2021"} />
-          <FiTrash className="eventsComponent-events_event--delete" color="#dc143c" size="25px" strokeWidth="2.5px" />
-        </div>
+      <div className="eventsComponent-title">Évènements</div>
+      <div className="eventsComponent-addEventForm">
+        <form onSubmit={handleSubmit(handleNewEventSubmit)} className="eventsComponent-addEventForm-form">
+          <input
+            {...register('eventName')}
+            control={control}
+            name="eventName"
+            rules={{ required: true }}
+            type="text"
+            className="eventsComponent-addEventForm-form_input"
+            placeholder="Nom de l'évènement"
+          />
+          <DatePicker
+            onChange={(date) => handleEventDateChange(date)}
+            className="eventsComponent-addEventForm-form_date"
+            showTimeSelect
+            selected={eventDate}
+          />
+          <input type="submit" value="+" className="eventsComponent-addEventForm-form_btn" />
+        </form>
       </div>
       <div className="eventsComponent-events">
         <div className="eventsComponent-events_event">
@@ -39,7 +83,15 @@ const EventsComponent = () => {
           <FiTrash className="eventsComponent-events_event--delete" color="#dc143c" size="25px" strokeWidth="2.5px" />
         </div>
       </div>
-    </div>
+      <div className="eventsComponent-events">
+        <div className="eventsComponent-events_event">
+          <div className="eventsComponent-events_event--title">Concert</div>
+          <div className="eventsComponent-events_event--date">01/01/2021</div>
+          <ExpirationChip expireDate={"01/01/2021"} />
+          <FiTrash className="eventsComponent-events_event--delete" color="#dc143c" size="25px" strokeWidth="2.5px" />
+        </div>
+      </div>
+    </div >
   );
 };
 
---
+++ src/components/Utils/AddButtonFilled/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+const AddButton = () => {
+  const isEmpty = true;
+
+  return (
+    <div className="addTaskBtnFilled">
+      <p>+</p>
+    </div>
+  );
+};
+
+export default AddButton;
